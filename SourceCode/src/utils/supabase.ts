f326c0f0b2248e08596e687fc973bce336ca53bd
Ghosty <Shubhanshs@hotmail.com>
2026-04-09 15:34:24 +0400

    update supabase.ts
    removed water project usages

--- SourceCode/src/utils/supabase.ts
+++ SourceCode/src/utils/supabase.ts
@@ -5,8 +5,8 @@ let supabaseInstance: any = null;
 
 export function getSupabaseClient() {
     if (!supabaseInstance) {
-        const supabaseUrl = 'https://teknhplqqyclyahqgnfa.supabase.co';
-        const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRla25ocGxxcXljbHlhaHFnbmZhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkzMTAwMjAsImV4cCI6MjA4NDg4NjAyMH0.YbIWHIw84ndYVq6HGuRpt2h7KUinORMqyeCaVzE6UL0';
+        const supabaseUrl = '';
+        const supabaseKey = '';
         
         supabaseInstance = createClient(supabaseUrl, supabaseKey);
     }
